68d7bd527b5c003c57906adc9b112ec7e39d04f6
andrescollazos <pipecollazos1@gmail.com>
2016-12-09 15:11:17 -0500

    Inicio de Sesión con nombre de usuario
    - Se ajusta un problema de tabulación del código anterior.
    - Cuando un jugador inicia, se sincroniza su hora local usando el algoritmo de Berkeley. Posteriormente se pide su nombre de usuario, este nombre puede ser pasado como un argumento del sistema

--- jugador.py
+++ jugador.py
@@ -3,30 +3,37 @@ import sys
 import time
 
 if __name__ == '__main__':
-	host, port = 'localhost', 5000
+    host, port = 'localhost', 5000
 
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	print "Conectandose a {0}:{1}".format(host, port)
-	try:
-		sock.connect((host, port))
-	except socket.error as e:
-		print(str(e))
-	try:
-		while True:
-			data  = sock.recv(1000).split(' ')
-			#print "Recibiendo tiempo del servidor: {0}".format(data)
-			if data[0] == 'get':
-				desface = time.time() - float(data[1])
-				#print "Tiempo local: {0}".format(desface)
-				sock.sendall(str(desface))
-				#print "Enviando tiempo al servidor"
-			elif data[0] == 'post':
-				tiempo_local = float(data[1])
-				print "TIEMPO NUEVO: {0}".format(tiempo_local)
-			elif data[0] == '':
-				break
-	except socket.error as e:
-		print(str(e))
-	finally:
-		print "Cerrando la conexion!"
-		sock.close()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print "Conectandose a {0}:{1}".format(host, port)
+    try:
+        sock.connect((host, port))
+    except socket.error as e:
+        print(str(e))
+    try:
+        while True:
+            data  = sock.recv(1000).split(' ')
+            #print "Recibiendo tiempo del servidor: {0}".format(data)
+            if data[0] == 'get':
+                desface = time.time() - float(data[1])
+                #print "Tiempo local: {0}".format(desface)
+                sock.sendall(str(desface))
+                #print "Enviando tiempo al servidor"
+            elif data[0] == 'post':
+                tiempo_local = float(data[1])
+                print "TIEMPO NUEVO: {0}".format(tiempo_local)
+            elif data[0] == '':
+                break
+            elif data[0] == 'name':
+                try:
+                    usuario = str(sys.argv[1])
+                except:
+                    usuario = raw_input("Digita tu nombre de usuario: ")
+                sock.sendall(usuario)
+
+    except socket.error as e:
+        print(str(e))
+    finally:
+        print "Cerrando la conexion!"
+        sock.close()
--- server.py
+++ server.py
@@ -2,56 +2,80 @@ import socket
 import time
 
 class TimeServer:
-	def __init__(self, host, port):
-		self.servidor_tiempo = time.time()
-		self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		try:
-			self.server_sock.bind((host, port))
-		except socket.error as e:
-		    print(str(e))
-		    sys.exit()
+    def __init__(self, host, port):
+        self.servidor_tiempo = time.time()
+        self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        try:
+            self.server_sock.bind((host, port))
+        except socket.error as e:
+            print(str(e))
+            sys.exit()
 
-		self.server_sock.listen(10)
-		self.lista_conexiones = [self.server_sock]
-		print "Esperando por conexiones {0}:{1}".format(host, port)
+        self.server_sock.listen(10)
+        self.limite_jugadores = 3
+        self.lista_conexiones = [self.server_sock]
+        self.jugadores = 0
+        self.lista_jugadores = {}
+        print "Esperando conexiones {0}:{1}".format(host, port)
 
-	def iniciar(self):
-		try:
-			while True:
-				self.aceptar_conexion()
-		except socket.error as e:
-			print "Error ", e
-		finally:
-			self.server_sock.close()
+    def iniciar(self):
+        try:
+            while True:
+                self.aceptar_conexion()
+        except socket.error as e:
+            print "Error ", # coding=utf-8
+        finally:
+            self.server_sock.close()
 
-	def sincronizar(self):
-		tiempo_acumulado = 0
-		tiempo_local = time.time()
-		for sock in self.lista_conexiones:
-			if sock != self.server_sock:
-				inicio = time.time()
-				sock.send("get " + str(time.time())) # send current time
-				cliente_desface = float(sock.recv(4094)) # + or -
-				fin = time.time()
-				cliente_desface += ((fin - inicio) / 2) # + (time of sending)
-				tiempo_acumulado += tiempo_local + cliente_desface
-				#print "client offset: {0}".format(cliente_desface)
-		#print "current_time: {0}".format(tiempo_local)
-		print "NUEVO TIEMPO: {0}".format(tiempo_acumulado)
-		avg = (tiempo_acumulado + tiempo_local) / (len(self.lista_conexiones))
-		for sock in self.lista_conexiones:
-			if sock != self.server_sock:
-				sock.send("post " + str(avg))
-		# also set local time
+    def sincronizar(self):
+        tiempo_acumulado = 0
+        tiempo_local = time.time()
+        for sock in self.lista_conexiones:
+            if sock != self.server_sock:
+                inicio = time.time()
+                sock.send("get " + str(time.time())) # send current time
+                cliente_desface = float(sock.recv(4094))
+                fin = time.time()
+                cliente_desface += ((fin - inicio) / 2) # + (time of sending)
+                tiempo_acumulado += tiempo_local + cliente_desface
+                #print "client offset: {0}".format(cliente_desface)
+        #print "current_time: {0}".format(tiempo_local)
+        print "NUEVO TIEMPO: {0}".format(tiempo_acumulado)
+        print "CANTIDAD DE JUGADORES: ", self.jugadores
+        avg = (tiempo_acumulado + tiempo_local) / (len(self.lista_conexiones))
+        for sock in self.lista_conexiones:
+            if sock != self.server_sock:
+                sock.send("post " + str(avg))
 
-	def aceptar_conexion(self):
-		new_sock, addr = self.server_sock.accept()
-		print "Nueva conexion de {0}".format(addr)
-		self.lista_conexiones.append(new_sock)
-		self.sincronizar()
+        if self.jugadores == self.limite_jugadores:
+            self.iniciar_partida()
 
+    def aceptar_conexion(self):
+        new_sock, addr = self.server_sock.accept()
+        if self.jugadores < self.limite_jugadores:
+            print "Nueva conexion de {0}".format(addr)
+            self.lista_conexiones.append(new_sock)
+            self.jugadores = self.jugadores + 1
+            self.sincronizar()
+        else:
+            print "NO SE ACEPTAN MAS CONEXIONES"
+            new_sock.send("")
+
+    def iniciar_partida(self):
+        #print "Lista de conectados:"
+        #print self.lista_conexiones
+        for sock in self.lista_conexiones:
+            if sock != self.server_sock:
+                sock.send("name .")
+                nombre_jugador = sock.recv(1024)
+                self.lista_jugadores.update({nombre_jugador:sock})
+
+        print "Lista de Jugadores: "
+        #print self.lista_jugadores
+        for jugador in self.lista_jugadores:
+            print jugador, ":", self.lista_jugadores[jugador]
 
 if __name__ == '__main__':
-		server = TimeServer('localhost', 5000)
-		server.iniciar()
+    server = TimeServer('localhost', 5000)
+    server.iniciar()
