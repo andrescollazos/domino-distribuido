4ade9682f2bcfce87add6feeff7b93eca24e023b
andrescollazos <pipecollazos1@gmail.com>
2016-12-11 14:27:36 -0500

    Configuracion Inicial de Juego
    - El server genera todas las combinaciones de fichas (28 fichas)
    - El server crea un hilo de juego para cada usuario.
    - Codigo Comentado

--- jugador.py
+++ jugador.py
@@ -1,9 +1,10 @@
 import socket
 import sys
 import time
+import pygame
 
 if __name__ == '__main__':
-    host, port = 'localhost', 5000
+    host, port = 'localhost', 3000
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print "Conectandose a {0}:{1}".format(host, port)
@@ -13,27 +14,43 @@ if __name__ == '__main__':
         print(str(e))
     try:
         while True:
+            # El jugador puede recibir diferentes mensajes:
+            # [orden, argumentos] -> Es la forma de los mensajes que se reciben
             data  = sock.recv(1000).split(' ')
-            #print "Recibiendo tiempo del servidor: {0}".format(data)
-            if data[0] == 'get':
+            #print "Recibiendo del servidor: {0}".format(data)
+            if data[0] == 'get':# Esta orden le indica al jugador que debe enviar
+                                # el desface entre su hora local y la del servidor
                 desface = time.time() - float(data[1])
-                #print "Tiempo local: {0}".format(desface)
                 sock.sendall(str(desface))
-                #print "Enviando tiempo al servidor"
-            elif data[0] == 'post':
+            elif data[0] == 'post': # Esta orden le indica al jugador que debe
+                                    # actualizar su hora local
                 tiempo_local = float(data[1])
                 print "TIEMPO NUEVO: {0}".format(tiempo_local)
-            elif data[0] == '':
-                break
-            elif data[0] == 'name':
+            elif data[0] == '': # Esta orden le indica al jugador que ha terminado
+                break           # Su conexion con el servidor
+            elif data[0] == 'name': # Esta orden le indica al jugador que debe
+                                    # Enviar su nombre de usuario al servidor
                 try:
+                    # El jugador puede pasar su nombre de usuario como un argumento
+                    # al correr el programa. Ej: python jugador.py "CAROLINA"
                     usuario = str(sys.argv[1])
                 except:
+                    # En caso contrario el jugador ingresara por teclado su usuario.
                     usuario = raw_input("Digita tu nombre de usuario: ")
                 sock.sendall(usuario)
-            elif data[0] == 'repetido':
+            elif data[0] == 'repetido': # Esta orden indica que debe enviar otro nombre de usuario
                 usuario = raw_input("Nombre de Usuario ya existe. Escoge otro: ")
                 sock.sendall(usuario)
+            elif data[0] == 'init': # Esta orden indica que puede inciar el juego
+                pygame.init()
+                dimension = data[1].split(",") # Recibe las dimensiones de la pantalla
+                dimension = (int(dimension[0]), int(dimension[1])) #Convierte a entero
+                pantalla = pygame.display.set_mode(dimension) # Crea la pantalla
+                pygame.display.set_caption("DOMINO") # Nombre de la ventana
+                pantalla.blit(pygame.image.load("img/inicio.png"), (0, 0)) # Imagen inicial
+                pygame.display.flip()
+
+                # Recibe sus fichas de juego
 
 
     except socket.error as e:
--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 import socket
 import time
+import pygame
+import sys
+import threading
 
 class TimeServer:
     def __init__(self, host, port):
+        self.host = host
+        self.port = port
         self.servidor_tiempo = time.time()
         self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -17,9 +22,25 @@ class TimeServer:
         self.lista_conexiones = [self.server_sock]
         self.jugadores = 0
         self.lista_jugadores = {}
-        print "Esperando conexiones {0}:{1}".format(host, port)
+        self.dim_pantalla = "800,600"
+        self.fichas = {}
+        #for ficha in self.fichas:
+        #    print ficha ," :", self.fichas[ficha]
 
+    # Este metodo permite iniciar el servidor, genera las fichas del juego y
+    # recibe las conexiones
     def iniciar(self):
+        print "Generar fichas de juego..."
+        for i in range(7):
+            for j in range(7):
+                ficha = str(i) + "," + str(j)
+                if not(ficha in self.fichas or (str(j) + "," + str(i)) in self.fichas):
+                    ruta_imagen = str(i) + ".png," + str(j) + ".png"
+                    self.fichas.update({ficha:ruta_imagen})
+                    print "#",
+        print "\nFichas generadas correctamente..."
+
+        print "Esperando jugadores {0}:{1}".format(self.host, self.port)
         try:
             while True:
                 self.aceptar_conexion()
@@ -28,43 +49,54 @@ class TimeServer:
         finally:
             self.server_sock.close()
 
+    # Este metodo acepta o rechaza una conexion, el servidor acepta una conexion
+    # cuando aun no se ha completado la cantidad maxima de jugadores, que son 4
+    # el servidor rechaza la conexion cuando hay ya cuatro jugadores
+    def aceptar_conexion(self):
+        new_sock, addr = self.server_sock.accept() # Recibe peticion de conexion
+        if self.jugadores < self.limite_jugadores: # Comprueba la cantidad de jugadores
+            print "Recibida conexion de Jugador #{1} : {0}".format(addr, self.jugadores + 1)
+            self.lista_conexiones.append(new_sock)
+            self.jugadores = self.jugadores + 1
+            self.sincronizar()  # Cada que se recibe una nueva conexion, el servidor
+                                # utiliza el algoritmo de Berkeley para sincronizar
+                                # la hora local de los jugadores.
+        else:
+            print "NO SE ACEPTAN MAS CONEXIONES"
+            new_sock.send("")
+
+    # Metodo basado en el algoritmo de Berkeley para la sincronizacion de la hora local
+    # de los jugadores
     def sincronizar(self):
         tiempo_acumulado = 0
         tiempo_local = time.time()
         for sock in self.lista_conexiones:
             if sock != self.server_sock:
                 inicio = time.time()
-                sock.send("get " + str(time.time())) # send current time
+                # El servidor envia su tiempo local al jugador
+                sock.send("get " + str(time.time()))
+                # Este lo responde con el desface
                 cliente_desface = float(sock.recv(4094))
                 fin = time.time()
-                cliente_desface += ((fin - inicio) / 2) # + (time of sending)
+                # Se agrega el tiempo que se demora en enviar y recibir la solicitud
+                cliente_desface += ((fin - inicio) / 2)
                 tiempo_acumulado += tiempo_local + cliente_desface
-                #print "client offset: {0}".format(cliente_desface)
-        #print "current_time: {0}".format(tiempo_local)
         print "NUEVO TIEMPO: {0}".format(tiempo_acumulado)
-        #print "CANTIDAD DE JUGADORES: ", self.jugadores
         avg = (tiempo_acumulado + tiempo_local) / (len(self.lista_conexiones))
         for sock in self.lista_conexiones:
             if sock != self.server_sock:
                 sock.send("post " + str(avg))
 
+        # En el caso de que se llegue al maximo de conexiones permitidas, el servidor
+        # dara inciio a la partida llamando al metodo iniciar_partida()
         if self.jugadores == self.limite_jugadores:
             self.iniciar_partida()
 
-    def aceptar_conexion(self):
-        new_sock, addr = self.server_sock.accept()
-        if self.jugadores < self.limite_jugadores:
-            print "Nueva conexion de {0}".format(addr)
-            self.lista_conexiones.append(new_sock)
-            self.jugadores = self.jugadores + 1
-            self.sincronizar()
-        else:
-            print "NO SE ACEPTAN MAS CONEXIONES"
-            new_sock.send("")
-
+    # Este metodo recolecta la informacion basica para iniciar la partida.
+    # Le pregunta a todos los conectado que nombre de usuario van a usar
+    # en caso de que este ya este, el servidor seguira preguntando hasta obtener
+    # un nombre valido
     def iniciar_partida(self):
-        #print "Lista de conectados:"
-        #print self.lista_conexiones
         for sock in self.lista_conexiones:
             if sock != self.server_sock:
                 sock.send("name .")
@@ -75,11 +107,29 @@ class TimeServer:
 
                 self.lista_jugadores.update({nombre_jugador:sock})
 
+        # MOSTRAR EN EL SERVIDOR LOS JUGADORES QUE SE CONECTARON
         print "Lista de Jugadores: "
         #print self.lista_jugadores
         for jugador in self.lista_jugadores:
             print jugador, ":", self.lista_jugadores[jugador]
 
+        # Una vez iniciado el conjunto de jugadores, se procede a llamar al metodo
+        # juego(), el cual permite la interaccion con cada uno de los jugadores
+        for sock_jugador in self.lista_jugadores:
+            # Se crea un hilo de juego para cada uno de los jugadores
+            jugador = threading.Thread(target = self.juego, args = (sock_jugador, ))
+            jugador.start() # Iniciar hilo
+
+    def juego(self, sock):
+        # El sock que es recibido, es la llave que apunta al socket en el conjunto de
+        # jugadores, nos interesa trabajar con el socket, no con la llave.
+        llave = sock
+        sock = self.lista_jugadores[llave]
+
+        # Se la manda al jugador las dimensiones de la pantalla del juego
+        if sock != self.server_sock:
+            sock.send("init " + self.dim_pantalla)
+
 if __name__ == '__main__':
-    server = TimeServer('localhost', 5000)
+    server = TimeServer('localhost', 3000)
     server.iniciar()
